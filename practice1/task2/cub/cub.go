2974e7d4f98d07fa9fb67198e0b5f50997658972
user <user@EPBYMOGW0281.minsk.epam.com>
2019-10-23 21:00:38 +0300

    practika1

--- practice1/task2/cub/cub.go
+++ practice1/task2/cub/cub.go
@@ -16,43 +16,46 @@ func Core(size int, arr []int) {
 		arr = append(arr, randomInt(1, 7)+randomInt(1, 7))
 		x++
 	}
-
-	//var x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12 int
-	x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12 := 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-	//sort.Ints(arr)
-	//fmt.Println(arr)
+	countarr := make([]int, 12)
 	for i := 0; i < size; i++ {
 
 		switch arr[i] {
 		case 2:
-			x2++
+			countarr[0]++
 		case 3:
-			x3++
+			countarr[1]++
 		case 4:
-			x4++
+			countarr[2]++
 		case 5:
-			x5++
+			countarr[3]++
 		case 6:
-			x6++
+			countarr[4]++
 		case 7:
-			x7++
+			countarr[5]++
 		case 8:
-			x8++
+			countarr[6]++
 		case 9:
-			x9++
+			countarr[7]++
 		case 10:
-			x10++
+			countarr[8]++
 		case 11:
-			x11++
+			countarr[9]++
 		case 12:
-			x12++
+			countarr[10]++
 		}
 
 	}
-	fmt.Println(float64(x2))
+	for j := range countarr {
+		fmt.Print(j + 2)
+		fmt.Print("---")
+		fmt.Printf("%.1f \n", (100 / float64(countarr[j])))
+
+		//fmt.Printf(" %v", inc(100/countarr[j]))
+	}
+
 	//var xx1 float64 = float64(100 / float64(x2))
 	//x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12 = 100/x2, 100/x3, 100/x4, 100/x5, 100/x6, 100/x7, 100/x8, 100/x9, 100/x10, 100/x11, 100/x12
-	fmt.Printf("%.1f", (100 / x2))
+	//	fmt.Printf("%.1f", (100 / x2))
 }
 
 func init() {
